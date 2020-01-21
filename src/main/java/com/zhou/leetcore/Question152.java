65fa31e35ef83b375e97692601412c64ac35f9ca
xiaohu <1209325963@qq.com>
2020-01-21 16:02:57 +0800

    leetcore题

---
+++ src/main/java/com/zhou/leetcore/Question152.java
@@ -0,0 +1,29 @@
+package com.zhou.leetcore;
+
+/**
+ * @Description 乘积最大子序列
+ * @Author xiaohu
+ * @Date 2020/1/21 9:31
+ */
+public class Question152 {
+
+    /**
+     给定一个整数数组 nums ，找出一个序列中乘积最大的连续子序列（该序列至少包含一个数）。
+     示例 1:
+     输入: [2,3,-2,4]
+     输出: 6
+     解释: 子数组 [2,3] 有最大乘积 6。
+     示例 2:
+     输入: [-2,0,-1]
+     输出: 0
+     解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+     */
+
+    /**
+     * @param nums
+     * @return
+     */
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+}
